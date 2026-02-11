e1a59a7d34b1cfdc34f19ea120ab594ab0c6cc5a
Wakoli Ian <wakoliian2007@gmail.com>
2026-02-11 07:31:25 +0000

    Update query `My first shared query`

--- 3199303-My first shared query.sql
+++ 3199303-My first shared query.sql
@@ -9,4 +9,4 @@ DROP TABLE student;
 ALTER TABLE student ADD school VARCHAR(20);
 ALTER TABLE student DROP gpa;
 SELECT * FROM student;
-INSERT INTO student VALUES(2, 'Professor Smalla', 'Co', 'Kirinyaga', 3.6 );
+INSERT INTO student VALUES(2, 'Professor Smalla', 'Computer science', 'Kirinyaga', .6 );
