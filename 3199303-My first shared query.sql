1c423a9664c11949f5e13670be3de9e2aeb7e930
Wakoli Ian <wakoliian2007@gmail.com>
2026-03-09 12:55:30 +0000

    Update query `My first shared query`

--- 3199303-My first shared query.sql
+++ 3199303-My first shared query.sql
@@ -1,8 +1,8 @@
 DROP TABLE student;
 CREATE TABLE student(
-    student_Id  INT  PRIMARY KEY AUTO_INCREMENT,
+    student_Id  INT  PRIMARY KEY,
     name  VARCHAR(20) NOT NULL,
-    major VARCHAR(20) UNIQUE DEFAULT 'Undecided',
+    major VARCHAR(20) UNIQUE,
 );
 DESCRIBE student;
 ALTER TABLE student ADD GPA DECIMAL(3,2);
@@ -10,15 +10,15 @@ DROP TABLE student;
 ALTER TABLE student ADD school VARCHAR(20);
 ALTER TABLE student DROP gpa;
 SELECT * FROM student;
-INSERT INTO student VALUES( 'Professor Smalla', 'Computer science', 'Kirinyaga', 4.6 );
-INSERT INTO student(student_Id, name, major, GPA) VALUES( 'Ian', 'Cyber security', 4.5);
-INSERT INTO student(student_Id, name, major, school) VALUES( 'Ian', 'Cyber security', 'kirinyaga');
-INSERT INTO student VALUES( 'Professor Smalla', 'Computer Engineering', 'Kirinyaga', 7.6 );
+INSERT INTO student VALUES(2, 'Professor Smalla', 'Computer science', 'Kirinyaga', 4.6 );
+INSERT INTO student(student_Id, name, major, GPA) VALUES(5, 'Ian', 'Cyber security', 4.5);
+INSERT INTO student(student_Id, name, major, school) VALUES(6, 'Ian', 'Cyber security', 'kirinyaga');
+INSERT INTO student VALUES(9, 'Professor Smalla', 'Computer Engineering', 'Kirinyaga', 7.6 );
 
 DROP TABLE Students;
 
 CREATE TABLE Students(
-    student_Id  INT AUTO_INCREMENT,
+    student_Id  INT,
     name  VARCHAR(20) NOT NULL,
     major VARCHAR(20) UNIQUE,
     PRIMARY KEY(student_Id)
@@ -28,7 +28,7 @@ ALTER TABLE Students ADD school VARCHAR(20);
 ALTER TABLE Students ADD GPA DECIMAL(3,2);
 
 SELECT * FROM Students;
-INSERT INTO Students VALUES( 1, 'Professor Smalla', 'Computer science', 'Kirinyaga', 4.6 );
-INSERT INTO Students(student_Id, name, major, GPA) VALUES( 'Ian', 'Cyber Technician', 4.5);
-INSERT INTO Students(student_Id, name, major, school) VALUES( 'Ian', 'Cyber security', 'kirinyaga');
-INSERT INTO Students VALUES( 'Professor Smalla', 'Computer Engineering', 'Kirinyaga', 7.6 );
+INSERT INTO Students VALUES(2, 'Professor Smalla', 'Computer science', 'Kirinyaga', 4.6 );
+INSERT INTO Students(student_Id, name, major, GPA) VALUES(5, 'Ian', 'Cyber Technician', 4.5);
+INSERT INTO Students(student_Id, name, major, school) VALUES(6, 'Ian', 'Cyber security', 'kirinyaga');
+INSERT INTO Students VALUES(9, 'Professor Smalla', 'Computer Engineering', 'Kirinyaga', 7.6 );
