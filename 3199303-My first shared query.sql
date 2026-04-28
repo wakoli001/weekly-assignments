49057529dccd555ffe658bd92c38c01a0ea2b8c4
Wakoli Ian <wakoliian2007@gmail.com>
2026-04-28 14:34:04 +0000

    Update query `My first shared query`

--- 3199303-My first shared query.sql
+++ 3199303-My first shared query.sql
@@ -28,7 +28,7 @@ ALTER TABLE Students ADD school VARCHAR(20);
 ALTER TABLE Students ADD GPA DECIMAL(3,2);
 
 SELECT * FROM Students;
-INSERT INTO Students VALUES(2, 'Professor Smalla', 'Computer science', 'Kirinyaga', 4.6 );
-INSERT INTO Students(student_Id, name, major, GPA) VALUES(5, 'Ian', 'Cyber Technician', 4.5);
+INSERT INTO Students VALUES(1, 'Professor Smalla', 'Computer science', 'Kirinyaga', 4.6 );
+INSERT INTO Students(student_Id, name, major, GPA) VALUES( 'Ian', 'Cyber Technician', 4.5);
 INSERT INTO Students(student_Id, name, major, school) VALUES(6, 'Ian', 'Cyber security', 'kirinyaga');
 INSERT INTO Students VALUES(9, 'Professor Smalla', 'Computer Engineering', 'Kirinyaga', 7.6 );
